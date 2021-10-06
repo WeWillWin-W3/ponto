b140cfa58538b0db9fa5976e6d1e1de8f69dada2
Lucas Barbosa <llbarbosasc@gmail.com>
2021-10-06 18:40:49 -0400

    fix(database): Corrige nomes das colunas nas queries

--- database/util.sql
+++ database/util.sql
@@ -12,7 +12,7 @@ CREATE FUNCTION abs(interval) RETURNS interval AS
   $$ SELECT CASE WHEN ($1<interval '0') THEN -$1 ELSE $1 END; $$
 LANGUAGE SQL IMMUTABLE;
 
-CREATE FUNCTION get_one_bits_num(r bit(7)) RETURNS interval AS
+CREATE FUNCTION get_one_bits_num(r bit(7)) RETURNS integer AS
     $$ SELECT length(regexp_replace(r::text, '[^1]', '', 'g')); $$
 LANGUAGE SQL IMMUTABLE
 RETURNS NULL ON NULL INPUT;
@@ -21,40 +21,37 @@ CREATE VIEW "UnscheduledAttendance" AS
     SELECT a.* FROM "Attendance" as a
     WHERE NOT EXISTS (
         SELECT TRUE FROM "WorkScheduleEmployee" as wse
-        JOIN "WorkSchedule" as ws ON ws.id = wse."workSchedule"
-        JOIN "WorkScheduleTimeRange" as wstr ON wstr."workSchedule" = ws.id
-        JOIN "TimeRange" as tr ON tr.id = wstr."timeRange"
+        JOIN "WorkSchedule" as ws ON ws.id = wse.work_schedule
+        JOIN "WorkScheduleTimeRange" as wstr ON wstr.work_schedule = ws.id
+        JOIN "TimeRange" as tr ON tr.id = wstr.time_range
         WHERE wse.employee = a.employee AND ws.workable = TRUE 
-            AND ((a.time::time BETWEEN tr.start AND tr.end) AND date_is_in_daterangebit(a.time::date, wstr."repeatDays")));
+            AND ((a.time::time BETWEEN tr.start AND tr.end) AND date_is_in_daterangebit(a.time::date, wstr.repeat_days)));
 
--- Attendance intervals
-WITH ordered_attendances AS (
-	SELECT row_number() OVER (PARTITION BY employee ORDER BY employee, time
-) - 1 AS i, * FROM "Attendance" ORDER BY employee, time)
-SELECT da.id AS start_id, da.time AS start, da2.id AS end_id, da2.time AS end, da.employee FROM ordered_attendances as da
-JOIN ordered_attendances da2 ON da2.i-1 = da.i AND da2.employee = da.employee
-WHERE da.i % 2 = 0;
+CREATE VIEW "AttendanceIntervals" AS (
+	WITH ordered_attendances AS (
+		SELECT row_number() OVER (PARTITION BY employee ORDER BY employee, time
+	) - 1 AS i, * FROM "Attendance" ORDER BY employee, time)
+	SELECT da.id AS start_id, da.time AS start, da2.id AS end_id, da2.time AS end, da.employee FROM ordered_attendances as da
+	JOIN ordered_attendances da2 ON da2.i-1 = da.i AND da2.employee = da.employee
+	WHERE da.i % 2 = 0
+);
 
 -- Employee TimeRanges
-SELECT wse.employee, ws.workable, wstr."repeatDays", tr.start, tr.end 
-FROM "WorkScheduleEmployee" as wse 
-JOIN "WorkSchedule" as ws ON ws.id = wse."workSchedule"
-JOIN "WorkScheduleTimeRange" as wstr ON wstr."workSchedule" = ws.id
-JOIN "TimeRange" as tr ON tr.id = wstr."timeRange"
-ORDER BY wse.employee;
+CREATE VIEW "EmployeeTimeRanges" AS (
+	SELECT wse.employee, ws.workable, wstr.repeat_days, tr.start, tr.end 
+	FROM "WorkScheduleEmployee" as wse 
+	JOIN "WorkSchedule" as ws ON ws.id = wse.work_schedule
+	JOIN "WorkScheduleTimeRange" as wstr ON wstr.work_schedule = ws.id
+	JOIN "TimeRange" as tr ON tr.id = wstr.time_range
+	ORDER BY wse.employee
+);
+
 
 -- Get closest TimeRange by attendance pair
 CREATE VIEW "MatchingTimeRangeByAttendancePair" AS (
-	WITH employee_timeranges AS (
-		SELECT wse.employee, ws.workable, ws.description, wstr."repeatDays", tr.* FROM "WorkScheduleEmployee" as wse 
-		JOIN "WorkSchedule" as ws ON ws.id = wse."workSchedule"
-		JOIN "WorkScheduleTimeRange" as wstr ON wstr."workSchedule" = ws.id
-		JOIN "TimeRange" as tr ON tr.id = wstr."timeRange"
-		ORDER BY wse.employee
-	)
 	SELECT inner_table.employee, start_attendance, start_attendance_time, end_attendance, end_attendance_time,  
 	matching_timerange, workable as timerange_workable, description AS timerange_workschedule_description, 
-	"repeatDays" as timerange_workschedule_repeat_days, "start" as timerange_start, "end" as timerange_end 
+	repeat_days as timerange_workschedule_repeat_days, "start" as timerange_start, "end" as timerange_end 
 	FROM (
 		WITH ordered_attendances AS (
 			SELECT row_number() OVER (PARTITION BY employee ORDER BY employee, time) - 1 AS i, * FROM "Attendance" AS a
@@ -63,8 +60,8 @@ CREATE VIEW "MatchingTimeRangeByAttendancePair" AS (
 		SELECT oa.id AS start_attendance, oa.time AS start_attendance_time, 
 		oa2.id AS end_attendance, oa2.time AS end_attendance_time, oa.employee, (
 			SELECT et.id
-			FROM employee_timeranges AS et
-			WHERE date_is_in_daterangebit(oa.time::date, et."repeatDays") OR date_is_in_daterangebit(oa2.time::date, et."repeatDays")
+			FROM "EmployeeTimeRanges" AS et
+			WHERE date_is_in_daterangebit(oa.time::date, et.repeat_days) OR date_is_in_daterangebit(oa2.time::date, et.repeat_days)
 			-- Prefer workable ranges, if exists
 			-- ORDER BY et.workable DESC, (ABS(et.start::time - oa.time::time) + ABS(et.end::time - oa2.time::time)) ASC
 			ORDER BY (ABS(et.start::time - oa.time::time) + ABS(et.end::time - oa2.time::time)) ASC, et.workable DESC
@@ -73,7 +70,7 @@ CREATE VIEW "MatchingTimeRangeByAttendancePair" AS (
 		JOIN ordered_attendances AS oa2 ON oa2.i-1 = oa.i AND oa2.employee = oa.employee
 		WHERE oa.i % 2 = 0
 	) AS inner_table
-	LEFT JOIN employee_timeranges AS et ON et.id = matching_timerange AND et.employee = inner_table.employee
+	LEFT JOIN "EmployeeTimeRanges" AS et ON et.id = matching_timerange AND et.employee = inner_table.employee
 	ORDER BY inner_table.employee, start_attendance
 );
 
@@ -95,14 +92,14 @@ end_attendance_time AS compensatory_time_end,
 
 -- Get employee work hours by workschedule
 SELECT employee, workschedule_id,
-SUM(CASE WHEN workable AND date_is_in_daterangebit(day, "repeatDays") THEN inner_table.end - inner_table.start ELSE '0'::interval END) as work_hours
+SUM(CASE WHEN workable AND date_is_in_daterangebit(day, repeat_days) THEN inner_table.end - inner_table.start ELSE '0'::interval END) as work_hours
 FROM (
- 	SELECT wse.employee, ws.workable, wstr."repeatDays", tr.id as timerange_id, tr.start, tr.end, 
-	generate_series('2021-08-01'::date, '2021-08-31', '1 day')::date as day, wse."workSchedule" as workschedule_id
+ 	SELECT wse.employee, ws.workable, wstr.repeat_days, tr.id as timerange_id, tr.start, tr.end, 
+	generate_series('2021-08-01'::date, '2021-08-31', '1 day')::date as day, wse.work_schedule as workschedule_id
 	FROM "WorkScheduleEmployee" as wse 
-	JOIN "WorkSchedule" as ws ON ws.id = wse."workSchedule"
-	JOIN "WorkScheduleTimeRange" as wstr ON wstr."workSchedule" = ws.id
-	JOIN "TimeRange" as tr ON tr.id = wstr."timeRange"
+	JOIN "WorkSchedule" as ws ON ws.id = wse.work_schedule
+	JOIN "WorkScheduleTimeRange" as wstr ON wstr.work_schedule = ws.id
+	JOIN "TimeRange" as tr ON tr.id = wstr.time_range
 	ORDER BY wse.employee
 ) AS inner_table
 GROUP BY employee, workschedule_id;
