45170892363d256f55688c8cce49ca3305ee5ebe
Lucas Barbosa <llbarbosasc@gmail.com>
2021-10-06 18:45:42 -0400

    fix(database): Corrige nomes das colunas nas queries
    Transforma também todas as queries auxiliares em Views

--- database/setup.sh
+++ database/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 psql -d ponto < /docker-entrypoint-initdb.d/sql/schema.sql
 psql -d ponto < /docker-entrypoint-initdb.d/sql/seed.sql
-psql -d ponto < /docker-entrypoint-initdb.d/sql/functions.sql
+psql -d ponto < /docker-entrypoint-initdb.d/sql/util.sql
